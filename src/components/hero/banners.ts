a7260692e0cbabd1aba89f2d6764a1e1a3a55cac
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-08-26 16:20:39 +0100

    feat: more banners

--- src/components/hero/banners.ts
+++ src/components/hero/banners.ts
@@ -2,6 +2,8 @@ const BANNERS = [
 	() => import("../../assets/banners/ffxiv_07072025_151021_661.png"),
 	() => import("../../assets/banners/ffxiv_12012025_214531_229.png"),
 	() => import("../../assets/banners/ffxiv_28052025_235242_761.png"),
+	() => import("../../assets/banners/ffxiv_17062025_222424_335.png"),
+	() => import("../../assets/banners/ffxiv_22032025_155245_007.png"),
 ];
 
 export function getRandomBanner() {
